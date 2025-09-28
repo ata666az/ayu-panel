40bc94f1a7b2b6c86387f8399ca8fbc16320b8d0
ata666az <ata666az@gmail.com>
2025-09-28 23:57:02 +0700

    Update worker-configuration.d.ts

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,6 +1,6 @@
 interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	// MY_KV_NAMESPACE: 55d19e22896c4320a7405df42f573f4f;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
